b57ca50a0dff726eea781e48bee1895d1d42e619
AlexanderF-R <alexferruarua564@gmail.com>
2026-03-03 03:24:34 -0500

    Swagger agregado

--- telco-ventas-backend/src/main/java/telcoventas/config/SecurityConfig.java
+++ telco-ventas-backend/src/main/java/telcoventas/config/SecurityConfig.java
@@ -32,6 +32,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/login").permitAll()
 
+                        // SWAGGER / OPENAPI (PUBLIC)
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+
                         // AGENTE
                         .requestMatchers(HttpMethod.POST, "/api/v1/ventas").hasRole("AGENTE")
                         .requestMatchers(HttpMethod.GET, "/api/v1/ventas/mis-ventas").hasRole("AGENTE")
